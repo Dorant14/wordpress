e3f160cdcbed8a34a51b79e202217a5aace2f2c5
dorant.hajdari12@gmail.com <Dorant14>
2025-06-10 07:03:19 -0700

    UPDATE

--- nr.1/function.php
+++ nr.1/function.php
@@ -10,11 +10,35 @@ function load_scripts(){
     );
 
     wp_enqueue_script('dropdown',get_tamplate_directory_uri().'/js/dropdown.js',array('jquery'),'1.0',true); 
-        
-        
 
 }
 
+    function config() {
+        register_nav_menus(
+            array(
+                'wp_devs_main_menu' => 'Main Menu',
+                'wp_devs_footer_menu' => 'Footer Menu',
+            )
+            );
+
+            $args = array(
+                'height' => 225.
+                'width' => 1920
+            );
+
+            add_theme_support('custom-header', $args);
+            add_theme_support('post-thumbnails');
+            add_theme_support('custom-logo', array(
+                'height' => 100,
+                'width' => 400,
+                'flex-height' => true,
+                'flex-width' => true,
+            ));
+            add_theme_support('automatic-feed-links');
+            add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script'));
+
+                
+    }
 
 add_action('wp_enqueue_scripts','load_scripts');
 
